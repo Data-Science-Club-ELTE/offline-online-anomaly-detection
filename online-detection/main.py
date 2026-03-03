10ad5e8e4b5c7a8d95b92cd782d237478dd42176
Matthew Balogh <meld-03-spouts@icloud.com>
2026-03-03 13:39:26 +0100

    Add `main.py`

---
+++ online-detection/main.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------------------------------------
+# IMPORTANT
+# --------------------------------------------------------------------------------------------------------
+#
+# This is the main development file.
+# Please make sure you only add justified code here, otherwise your code may be rejected by the reviewers.
+# For exploratory and experimental code, please use the `online-detection/tasks` folder.
+#
+# --------------------------------------------------------------------------------------------------------
+
