14551d203d371f6380b58de772396238eb5e3f77
Matthew Balogh <meld-03-spouts@icloud.com>
2026-03-10 00:18:35 +0100

    Polish first version of pipeline and add it to `main.py`

--- online-detection/main.py
+++ online-detection/main.py
@@ -8,11 +8,47 @@
 #
 # --------------------------------------------------------------------------------------------------------
 
+from river import datasets, metrics, compose, preprocessing, anomaly
+
 VERBOSE = True
+PROCESS_N_OBSERVATIONS = 2000
+
+if VERBOSE: print("\nStarting online anomaly detection on the Credit Card dataset...\n")
+
 
 # Load dataset
 
-from river.datasets import CreditCard
-
-dataset = CreditCard()
+dataset = datasets.CreditCard()
 if VERBOSE: print("Is the dataset downloaded? ", dataset.is_downloaded, "\n")
+
+
+# Evaluation metrics
+
+auc = metrics.ROCAUC()
+if VERBOSE: print("Using metrics of ROC-AUC to measure performance.\n")
+
+
+# Model pipeline
+    
+model = compose.Pipeline(
+    preprocessing.MinMaxScaler(),
+    anomaly.HalfSpaceTrees()
+)
+if VERBOSE: print("Pipeline initialized with MinMaxScaler and HalfSpaceTrees.\n")
+
+
+# Process the dataset and measure the performance
+
+if VERBOSE:
+    print("Starting to process the dataset and measure performance...\n")
+    if PROCESS_N_OBSERVATIONS is not None: print(f"Processing only the first {PROCESS_N_OBSERVATIONS} samples...\n")
+    else: print("Processing the entire dataset. This may take a while...\n")
+
+for x, y in dataset.take(PROCESS_N_OBSERVATIONS):
+    score = model.score_one(x)
+    model.learn_one(x)
+    auc.update(y, score)
+
+
+# Final evaluation
+print(f"Final ROC-AUC: {auc.get():.4f}")
