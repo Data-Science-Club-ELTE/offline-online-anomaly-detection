7644dabd5db7f3e6f13caa3b913d0b14a1e49325
Matthew Balogh <meld-03-spouts@icloud.com>
2026-03-11 11:23:16 +0100

    Add skeleton for offline pipeline

--- offline-detection/main.py
+++ offline-detection/main.py
@@ -8,3 +8,65 @@
 #
 # --------------------------------------------------------------------------------------------------------
 
+# Imports and constants
+from sklearn.ensemble import IsolationForest
+
+RANDOM_STATE = 42
+VERBOSE = True
+
+# 1. Load data
+
+def load_data():
+    # TODO: Load the dataset and detach target variable from the data.
+    # Output: data, target
+    data, target = None, None
+    return data, target
+
+
+# 2. Preprocessing
+
+def preprocess_data(data):
+    # TODO: Preprocess the data and convert into NumPy matrix format.
+    # steps: cleaning, encoding, scaling, feature dropping, etc.
+    # Output: X
+    X = None
+    return X
+
+
+# 3. Modeling
+
+def modeling(X):
+    # TODO: Fit the Isolation Forest algorithm on the preprocessed data.
+    # TODO: Settle on the initial hyperparameters of the model
+    # Output: model
+    hyparams = {"random_state": RANDOM_STATE, "verbose": VERBOSE}
+    model = IsolationForest(**hyparams)
+    return model
+
+
+# 4. Prediction
+
+def predict(model, X):
+    # TODO: Use the fitted model to make predictions on the same data that it was fitted on.
+    # Output: y_pred
+    y_pred = None
+    return y_pred
+
+
+# 5. Evaluation
+
+def evaluate(y_pred, target):
+    # TODO: Evaluate the performance of the model using appropriate metrics.
+    # Output: metrics
+    metrics = None
+    return metrics
+
+
+# Actual pipeline execution
+
+def pipeline():
+    if VERBOSE: print("Starting pipeline execution...")
+    # TODO: call methods in order ...
+    if VERBOSE: print("Pipeline execution completed.")
+
+pipeline()
