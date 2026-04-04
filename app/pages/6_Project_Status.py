ab24b6dd6a0ae71a1460410a4cedb91f1c03399e
Matthew Balogh <meld-03-spouts@icloud.com>
2026-04-04 04:53:37 +0200

    Add status report

--- app/pages/6_Project_Status.py
+++ app/pages/6_Project_Status.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import streamlit as st
 
 from branding import render_logo
@@ -10,7 +12,26 @@ st.set_page_config(
 
 render_logo()
 
+WORK_PRODUCTS_DIR = Path(__file__).resolve().parent.parent / "work_products"
+
 st.title(":material/detector: Project Status")
+
+st.container(height=25, border=False)
+
+st.caption("4th April | Before \"Final week\"")
+
+st.markdown("##### What we have done so far")
+st.image(WORK_PRODUCTS_DIR / "status_report_workflow.png")
+st.write("**Traditional Modeling:** In the traditional offline setting, the data was observable in its entirety. Detailed analysis could be performed before reporting anomalies. Challenge lied in high-dimensionality, lack of labels due to rare, expensive nature of anomalies.")
+
+st.write("**Stream Modeling:** In the streamlined online setting, the data was not observable in its entirety, it is iteratively changing. Detailed analysis could not be performed before reporting anomalies. Assessing anomalousness of new instances and updating a model in an iterative fashion was a challenge.")
+
+st.markdown("##### Next final steps")
+st.write("Both teams separate a validation set from the dataset, and use it to refine the models. Final performance evaluation.")
+
+st.markdown("##### Future directions")
+st.write("**A)** Combine the pipelines into a hybrid structure.")
+st.write("**B)** Perform a stress test, in which we inject synthetic data into anomalous regions, to see if the _Isolation Forest_ and _Half-Space Trees_ get confused and miss the original anomalies.")
 
 st.container(height=25, border=False)
 
